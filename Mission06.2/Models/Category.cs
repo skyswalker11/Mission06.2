06d6564476fff2b11f306155b2d7c942d93d6ec3
admin <you@example.com>
2022-02-09 18:21:16 -0700

    first commit

---
+++ Mission06.2/Models/Category.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//public class Class1
+//{
+//	public Class1()
+//	{
+//	}
+//}
+
+//new code here
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _413Mission06.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
